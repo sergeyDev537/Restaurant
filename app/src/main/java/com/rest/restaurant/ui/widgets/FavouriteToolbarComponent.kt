9f8f214bf8c00a808a5b92d36ccaa5a85ebc0d11
Sergey <sergiksavonow82@gmail.com>
2024-07-17 16:09:17 +0300

    Add custom widget for counter favourites restaurants

---
+++ app/src/main/java/com/rest/restaurant/ui/widgets/FavouriteToolbarComponent.kt
@@ -0,0 +1,63 @@
+package com.rest.restaurant.ui.widgets
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.rest.restaurant.ui.theme.AccentBlue
+
+@Composable
+fun FavouriteToolbarComponent(
+    count: Int,
+    isFilled: Boolean,
+) {
+    val text = if (count >= 100) { "∞" } else { count.toString() }
+    val textColor = if (isFilled) { Color.White } else { AccentBlue }
+
+    Box(
+        contentAlignment = Alignment.Center,
+    ) {
+        LikeComponent(
+            isLiked = isFilled,
+            onLike = {}
+        )
+        Text(
+            text = text,
+            color = textColor,
+            fontSize = 12.sp,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview
+@Composable
+fun FavouriteToolbarComponentPreview() {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(15.dp),
+    ) {
+        FavouriteToolbarComponent(
+            count = 17,
+            isFilled = true
+        )
+        FavouriteToolbarComponent(
+            count = 17,
+            isFilled = false
+        )
+        FavouriteToolbarComponent(
+            count = 100,
+            isFilled = true
+        )
+        FavouriteToolbarComponent(
+            count = 100,
+            isFilled = false
+        )
+    }
+}
