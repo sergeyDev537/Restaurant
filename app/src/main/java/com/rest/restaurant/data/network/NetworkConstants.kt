bfcf38eb30575f8f70ffbbcf67a38a352c67b44b
Sergey <sergiksavonow82@gmail.com>
2024-07-22 10:09:40 +0300

    Update API token

--- app/src/main/java/com/rest/restaurant/data/network/NetworkConstants.kt
+++ app/src/main/java/com/rest/restaurant/data/network/NetworkConstants.kt
@@ -2,6 +2,6 @@ package com.rest.restaurant.data.network
 
 object NetworkConstants {
 
-    const val AUTH_TOKEN = "Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NjksImV4cCI6MTcyMTM1Mjk4Mn0.MN9CdkO9oHukt0q9Crfq3zvj22q52sJbKKz3Tf59pBM"
+    const val AUTH_TOKEN = "Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NjksImV4cCI6MTcyMjAwMTc2Mn0.sq-txekB9d303-UXpOexJGM_js1U45vFoFLw6fwaBwk"
 
 }
